ad9aa6fd6de7bdfbac6f76d825204ea08d553ee4
Felix Auringer <48409110+felixauringer@users.noreply.github.com>
2021-04-27 10:05:30 +0200

    Split up the NewRouter function to ease the creation of versioned routers

--- api/api.go
+++ api/api.go
@@ -7,17 +7,23 @@ import (
 	"net/http"
 )
 
-// NewRouter returns an HTTP handler (http.Handler) which can then
-// be used by the net/http package to serve the api of our API.
-// We use gorilla/mux because it is more convenient than net/http,
-// e.g. when extracting path parameters.
+// NewRouter returns an HTTP handler (http.Handler) which can be
+// used by the net/http package to serve the routes of our API. It
+// always returns a router for the newest version of our API. We
+// use gorilla/mux because it is more convenient than net/http, e.g.
+// when extracting path parameters.
 func NewRouter() *mux.Router {
 	router := mux.NewRouter()
 	// this can later be restricted to a specific host with
 	// `router.Host(...)` and to HTTPS with `router.Schemes("https")`
+	return newRouterV1(router)
+}
+
+// newRouterV1 returns a subrouter containing the routes of version
+// 1 of our API.
+func newRouterV1(router *mux.Router) *mux.Router {
 	v1 := router.PathPrefix("/api/v1").Subrouter()
 	v1.HandleFunc("/health", Health).Methods(http.MethodGet)
-
 	return v1
 }
 

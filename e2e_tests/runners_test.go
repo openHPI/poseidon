ec1968ecf5898621f9ebc40ff888cda7775c8388
Jan-Eric Hellenberg <janeric.hellenberg@gmail.com>
2021-05-07 10:12:42 +0200

    Add e2e-test for delete runner route

--- e2e_tests/runners_test.go
+++ e2e_tests/runners_test.go
@@ -11,7 +11,8 @@ import (
 )
 
 func TestProvideRunnerRoute(t *testing.T) {
-	reader := strings.NewReader(`{"executionEnvironmentId":0}`)
+	runnerRequestString, _ := json.Marshal(dto.RunnerRequest{})
+	reader := strings.NewReader(string(runnerRequestString))
 	resp, err := http.Post(buildURL(api.RouteRunners), "application/json", reader)
 	assert.NoError(t, err)
 	assert.Equal(t, http.StatusOK, resp.StatusCode, "The response code should be ok")
@@ -21,4 +22,37 @@ func TestProvideRunnerRoute(t *testing.T) {
 	assert.NoError(t, err)
 
 	assert.True(t, runnerResponse.Id != "", "The response contains a runner id")
+}
+
+func newRunnerId(t *testing.T) string {
+	runnerRequestString, _ := json.Marshal(dto.RunnerRequest{})
+	reader := strings.NewReader(string(runnerRequestString))
+	resp, err := http.Post(buildURL(api.RouteRunners), "application/json", reader)
+	assert.NoError(t, err)
+	runnerResponse := new(dto.RunnerResponse)
+	_ = json.NewDecoder(resp.Body).Decode(runnerResponse)
+	return runnerResponse.Id
+}
+
+func TestDeleteRunnerRoute(t *testing.T) {
+	runnerId := newRunnerId(t)
+	assert.NotEqual(t, "", runnerId)
+
+	t.Run("Deleting the runner returns NoContent", func(t *testing.T) {
+		resp, err := httpDelete(buildURL(api.RouteRunners, "/", runnerId), nil)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusNoContent, resp.StatusCode)
+	})
+
+	t.Run("Deleting it again returns NotFound", func(t *testing.T) {
+		resp, err := httpDelete(buildURL(api.RouteRunners, "/", runnerId), nil)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusNotFound, resp.StatusCode)
+	})
+
+	t.Run("Deleting non-existing runner returns NotFound", func(t *testing.T) {
+		resp, err := httpDelete(buildURL(api.RouteRunners, "/", "n0n-3x1st1ng-1d"), nil)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusNotFound, resp.StatusCode)
+	})
 }

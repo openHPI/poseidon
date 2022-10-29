4393a088ba9c7eb31641ace1c3044d9e8b4c94ee
Maximilian Paß <22845248+mpass99@users.noreply.github.com>
2022-10-29 19:36:25 +0100

    Implement pull request comments #200.
    Co-authored-by: Sebastian Serth <mrserth@users.noreply.github.com>

--- deploy/grafana-dashboard/main.py
+++ deploy/grafana-dashboard/main.py
@@ -2,4 +2,4 @@
 from grafanalib._gen import generate_dashboard
 
 if __name__ == '__main__':
-    generate_dashboard(args=['-o', 'main.json', 'poseidon.dashboard.py'])
+    generate_dashboard(args=['-o', 'main.json', 'panels/poseidon.dashboard.py'])

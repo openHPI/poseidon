e1116b2f365a67b0343c8e9eaa65309fe29bef50
Maximilian Paß <22845248+mpass99@users.noreply.github.com>
2022-11-25 14:32:31 +0000

    Extract static environment overwrite into json file.

--- deploy/grafana-dashboard/utils/color_mapping.py
+++ deploy/grafana-dashboard/utils/color_mapping.py
@@ -1,5 +1,5 @@
 from utils.utils import deep_update_dict
-from functools import reduce
+import json
 
 
 def color_mapping(name, color):
@@ -23,23 +23,13 @@ def color_mapping(name, color):
 
 
 grey_all_mapping = color_mapping("all", "#4c4b5a")
+color_mapping_environments = {}
+colours = ["yellow", "blue", "orange", "red", "purple",
+           "pink", "brown", "black", "white", "gray",
+           "gold", "super-light-red", "dark-red", "dark-orange", "super-light-yellow",
+           "super-light-green", "dark-green", "dark-blue", "super-light-purple", "super-light-blue"]
 
-
-def add_color_mapping(mapping_dict, new_item):
-    deep_update_dict(mapping_dict, color_mapping(new_item[0], new_item[1]))
-    return mapping_dict
-
-
-color_mapping_environments = reduce(add_color_mapping, [
-    ("p10/java:8-antlr", "yellow"),
-    ("p28/r:4", "blue"),
-    ("p29/python:3.8", "orange"),
-    ("p31/java:17", "red"),
-    ("p33/openhpi/docker_exec_phusion", "purple"),
-    ("p11/java:8-antlr", "pink"),
-    ("p14/python:3.4", "brown"),
-    ("p18/node:0.12", "black"),
-    ("p22/python:3.4-rpi-web", "white"),
-    ("p25/ruby:2.5", "gray"),
-    ("p30/python:3.7-ml", "gold"),
-], {})
+with open("environments.json") as f:
+    environments = json.load(f)
+    for environment in environments:
+        deep_update_dict(color_mapping_environments, color_mapping(environment, colours.pop(0)))

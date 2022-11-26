a4b0d26c84f8ddf273d6e9ace9652d82c9cee79d
Maximilian Paß <22845248+mpass99@users.noreply.github.com>
2022-11-26 13:41:47 +0000

    Adjust dashboard colour mapping
    - Different colours
    - Sorted environment list
    
    Co-authored-by: Sebastian Serth <MrSerth@users.noreply.github.com>

--- deploy/grafana-dashboard/utils/color_mapping.py
+++ deploy/grafana-dashboard/utils/color_mapping.py
@@ -24,12 +24,13 @@ def color_mapping(name, color):
 
 grey_all_mapping = color_mapping("all", "#4c4b5a")
 color_mapping_environments = {}
-colours = ["yellow", "blue", "orange", "red", "purple",
-           "pink", "brown", "black", "white", "gray",
-           "gold", "super-light-red", "dark-red", "dark-orange", "super-light-yellow",
-           "super-light-green", "dark-green", "dark-blue", "super-light-purple", "super-light-blue"]
+colours = [
+            "#E41A1C", "#377EB8", "#4DAF4A", "#984EA3", "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999", # R ColorBrewer Set1
+            "#8DD3C7", "#FFFFB3", "#BEBADA", "#FB8072", "#80B1D3", "#FDB462", "#B3DE69", "#FCCDE5", "#D9D9D9", "#BC80BD", "#CCEBC5", "#FFED6F" # R ColorBrewer Set3
+]
 
 with open("environments.json") as f:
     environments = json.load(f)
+    environments.sort()
     for environment in environments:
         deep_update_dict(color_mapping_environments, color_mapping(environment, colours.pop(0)))

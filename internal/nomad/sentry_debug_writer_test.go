487ac69239c6601bb921a14c8c11782edf9a355b
Maximilian Paß <22845248+mpass99@users.noreply.github.com>
2023-02-26 21:28:30 +0000

    Ensure sending of the Sentry End debug message.

--- internal/nomad/sentry_debug_writer_test.go
+++ internal/nomad/sentry_debug_writer_test.go
@@ -20,3 +20,25 @@ func TestSentryDebugWriter_Write(t *testing.T) {
 	assert.Equal(t, len(data), count)
 	assert.NotContains(t, buf.String(), description)
 }
+
+func TestSentryDebugWriter_Close(t *testing.T) {
+	buf := &bytes.Buffer{}
+	s := NewSentryDebugWriter(buf, context.Background())
+	require.Empty(t, s.lastSpan.Tags)
+
+	s.Close(42)
+	require.Contains(t, s.lastSpan.Tags, "exit_code")
+	assert.Equal(t, "42", s.lastSpan.Tags["exit_code"])
+}
+
+func TestSentryDebugWriter_handleTimeDebugMessage(t *testing.T) {
+	buf := &bytes.Buffer{}
+	s := NewSentryDebugWriter(buf, context.Background())
+	require.Equal(t, "nomad.execute.connect", s.lastSpan.Op)
+
+	description := "TestDebugMessageDescription"
+	match := map[string][]byte{"time": []byte("1676646791482"), "text": []byte(description)}
+	s.handleTimeDebugMessage(match)
+	assert.Equal(t, "nomad.execute.bash", s.lastSpan.Op)
+	assert.Equal(t, description, s.lastSpan.Description)
+}

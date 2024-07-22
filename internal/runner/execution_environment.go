226195b10a13b79c023337c4f6ec59545aab3ba6
Maximilian Pass <22845248+mpass99@users.noreply.github.com>
2024-07-22 17:34:32 +0200

    Fix interfacebloat linter

--- internal/runner/execution_environment.go
+++ internal/runner/execution_environment.go
@@ -9,35 +9,52 @@ import (
 	"github.com/openHPI/poseidon/pkg/storage"
 )
 
-// ExecutionEnvironment are groups of runner that share the configuration stored in the environment.
-type ExecutionEnvironment interface {
-	json.Marshaler
-
+// ReadableExecutionEnvironment defines structs that can return the attributes of an Execution Environment.
+type ReadableExecutionEnvironment interface {
 	// ID returns the id of the environment.
 	ID() dto.EnvironmentID
-	SetID(id dto.EnvironmentID)
+
 	// PrewarmingPoolSize sets the number of idle runner of this environment that should be prewarmed.
 	PrewarmingPoolSize() uint
-	SetPrewarmingPoolSize(count uint)
-	// ApplyPrewarmingPoolSize creates idle runners according to the PrewarmingPoolSize.
-	ApplyPrewarmingPoolSize() error
+
 	// CPULimit sets the share of cpu that a runner should receive at minimum.
 	CPULimit() uint
-	SetCPULimit(limit uint)
+
 	// MemoryLimit sets the amount of memory that should be available for each runner.
 	MemoryLimit() uint
-	SetMemoryLimit(limit uint)
+
 	// Image sets the image of the runner, e.g. Docker image.
 	Image() string
-	SetImage(image string)
+
 	// NetworkAccess sets if a runner should have network access and if ports should be mapped.
 	NetworkAccess() (bool, []uint16)
+
+	// IdleRunnerCount returns the number of idle runners of the environment.
+	IdleRunnerCount() uint
+}
+
+// WriteableExecutionEnvironment defines structs that can update the attributes of an Execution Environment.
+type WriteableExecutionEnvironment interface {
+	SetID(id dto.EnvironmentID)
+	SetPrewarmingPoolSize(count uint)
+	SetCPULimit(limit uint)
+	SetMemoryLimit(limit uint)
+	SetImage(image string)
 	SetNetworkAccess(allow bool, ports []uint16)
+
 	// SetConfigFrom copies all above attributes from the passed environment to the object itself.
 	SetConfigFrom(environment ExecutionEnvironment)
 
+	// AddRunner adds an existing runner to the idle runners of the environment.
+	AddRunner(r Runner)
+}
+
+// ExternalExecutionEnvironment defines the functionalities with impact on external executors.
+type ExternalExecutionEnvironment interface {
 	// Register saves this environment at the executor.
 	Register() error
+	// ApplyPrewarmingPoolSize creates idle runners according to the PrewarmingPoolSize.
+	ApplyPrewarmingPoolSize() error
 	// Delete removes this environment and all it's runner from the executor and Poseidon itself.
 	// Iff local the environment is just removed from Poseidon without external escalation.
 	Delete(reason DestroyReason) error
@@ -45,14 +62,19 @@ type ExecutionEnvironment interface {
 	// Sample returns and removes an arbitrary available runner.
 	// ok is true iff a runner was returned.
 	Sample() (r Runner, ok bool)
-	// AddRunner adds an existing runner to the idle runners of the environment.
-	AddRunner(r Runner)
 	// DeleteRunner removes an idle runner from the environment and returns it.
 	// This function handles only the environment. The runner has to be destroyed separately.
 	// ok is true iff the runner was found (and deleted).
 	DeleteRunner(id string) (r Runner, ok bool)
-	// IdleRunnerCount returns the number of idle runners of the environment.
-	IdleRunnerCount() uint
+}
+
+// ExecutionEnvironment are groups of runner that share the configuration stored in the environment.
+type ExecutionEnvironment interface {
+	json.Marshaler
+
+	ReadableExecutionEnvironment
+	WriteableExecutionEnvironment
+	ExternalExecutionEnvironment
 }
 
 // monitorEnvironmentData passes the configuration of the environment e into the monitoring Point p.

20341789592df48781d3186387aa80d92770c7bd
Sebastian Serth <Sebastian.Serth@hpi.de>
2024-10-22 19:16:01 +0200

    Reintroduce custom fingerprinting for unexpected EOF
    The "unexpected EOF" error continues to exist and we should investigate why. Interestingly, it only happens when listing files, not for any other action.

--- internal/nomad/api_querier.go
+++ internal/nomad/api_querier.go
@@ -134,6 +134,12 @@ func (nc *nomadAPIClient) executeInAllocation(ctx context.Context, cmd string, a
 	case errors.Is(err, context.Canceled):
 		log.WithContext(ctx).Debug("Execution canceled by context")
 		return 0, nil
+	case errors.Is(err, io.ErrUnexpectedEOF), strings.Contains(err.Error(), io.ErrUnexpectedEOF.Error()):
+		// The unexpected EOF is a generic Nomad error. Because this error happens at the very end,
+		// it does not affect the functionality. Therefore, we don't propagate the error.
+		log.WithContext(ctx).WithError(err).
+			WithField(logging.SentryFingerprintFieldKey, []string{"nomad-unexpected-eof"}).Warn("Unexpected EOF for Execute")
+		return 0, nil
 	case strings.Contains(err.Error(), "Unknown allocation"):
 		return 1, ErrNomadUnknownAllocation
 	default:
